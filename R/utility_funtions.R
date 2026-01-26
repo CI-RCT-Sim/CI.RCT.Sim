381e48339f2e62024c35e33571f03a540ddefc34
Fellinger Tobias <tobias.fellinger@ages.at>
2026-01-26 17:00:50 +0100

    added support for list columns to params_scenarios_grid

--- R/utility_funtions.R
+++ R/utility_funtions.R
@@ -4,33 +4,46 @@
 #' @param ... vectors of parameter values
 #'
 #' @returns
-#' Returns a data.frame of parameter values for the simulations.
+#' Returns a tibble of parameter values for the simulations.
 #'
 #' @details
 #' The first value of each argument is taken to be the reference value. In
 #' addition to the reference scenario the output contains one line per
-#' additional value in each of the input arugments. In each of those lines only
+#' additional value in each of the input arguments In each of those lines only
 #' one parameter is varied. Each line differs from the first line in exactly one
 #' column.
 #'
+#' List columns can be used as they can be used in tibbles.
+#'
 #' @export
+#' @importFrom tibble tibble as_tibble
 #'
 #' @examples
 #' params_scenarios_grid(x=1:3, y=11:12, z=21)
+#' params_scenarios_grid(x=1:2, y=list(c(1), c(1, 2)))
 params_scenarios_grid <- function(...){
   params_args <- list(...)
   params_ref <- purrr::map(params_args, \(x){
     x[1]
   }) |>
-    as.data.frame()
+    tibble::as_tibble()
 
   params_other <- purrr::imap(params_args, \(x, i){
-    purrr::map(x[-1], \(y){
-      tmp <- params_ref
-      tmp[,i] <- y
-      tmp
+    if(class(x)=="list"){
+      purrr::map(x[-1], \(y){
+        tmp <- params_ref
+        tmp[,i] <- list(list(y))
+        tmp
     }) |>
       purrr::list_rbind()
+    } else {
+      purrr::map(x[-1], \(y){
+        tmp <- params_ref
+        tmp[,i] <- y
+        tmp
+      }) |>
+        purrr::list_rbind()
+    }
   }) |>
     purrr::list_rbind()
 
--- tests/testthat/test-utility_funtions.R
+++ tests/testthat/test-utility_funtions.R
@@ -1,10 +1,18 @@
 test_that("generating grid of simulation parameters works", {
   example_value <- params_scenarios_grid(x=1:3, y=letters[1:3], z=factor("A"))
-  expected_value <- data.frame(
+  expected_value <- tibble::tibble(
     x=c(1,2,3,1,1),
     y=letters[c(1,1,1,2,3)],
     z=factor("A")
   )
 
   expect_equal(example_value, expected_value)
+
+  example_value_list <- params_scenarios_grid(x=1:2, y=list(c(1), c(1, 2)))
+  expected_value_list <- tibble::tibble(
+    x=c(1,2,1),
+    y=list(c(1), c(1), c(1,2))
+  )
+
+  expect_equal(example_value_list, expected_value_list)
 })
