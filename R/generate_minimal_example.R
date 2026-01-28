060e8c334850b2f622f2f2b542fd865a4246decf
Fellinger Tobias <tobias.fellinger@ages.at>
2026-01-28 13:35:43 +0100

    Added vignette with minimal example and some explanations

--- R/generate_minimal_example.R
+++ R/generate_minimal_example.R
@@ -1,5 +1,36 @@
+#' Generate dataset for minimal example
+#'
+#' @param condition row of Design dataset
+#' @param fixed_objects list of parameters that are fixed across simulations
+#'
+#' @returns a simulated dataset
+#' @export
+#'
+#' @examples
+#' Design <- assumptions_minimal_example() |>
+#' true_summary_statistics_minimal_example()
+#'
+#' dat <- generate_minimal_example(Design[1,])
+#' head(dat)
+generate_minimal_example <- function(condition, fixed_objects=NULL){
+  data.frame(
+    group = c(rep(1, condition$n), rep(0, condition$n)),
+    y = c(rnorm(condition$n, condition$mean1), rnorm(condition$n, condition$mean0))
+  )
+}
 
+#' Calculate true summary statistics value for generate_minimal_example
+#'
+#' @param Design
+#'
+#' @returns For true_summary_statistics_minimal_example: the Design tibble with added column eff_size
+#' @export
+#' @describeIn generate_minimal_example calculate true effect size
+true_summary_statistics_minimal_example <- function(Design){
+  Design$eff_size <- Design$mean1 - Design$mean0
 
+  Design
+}
 
 #' Create an empty assumtions tibble for generate_minimal_example
 #'
@@ -26,38 +57,3 @@ assumptions_minimal_example <- function(print=interactive()){
   )
 }
 
-
-#' Calculate true summary statistics value for generate_minimal_example
-#'
-#' @param Design
-#'
-#' @returns For true_summary_statistics_minimal_example: the Design tibble with added column eff_size
-#' @export
-#' @describeIn generate_minimal_example calculate true effect size
-true_summary_statistics_minimal_example <- function(Design){
-  Design$eff_size <- Design$mean1 - Design$mean0
-
-  Design
-}
-
-
-#' Generate dataset for minimal example
-#'
-#' @param condition row of Design dataset
-#' @param fixed_objects list of parameters that are fixed across simulations
-#'
-#' @returns a simulated dataset
-#' @export
-#'
-#' @examples
-#' Design <- assumptions_minimal_example() |>
-#' true_summary_statistics_minimal_example()
-#'
-#' dat <- generate_minimal_example(Design[1,])
-#' head(dat)
-generate_minimal_example <- function(condition, fixed_objects=NULL){
-  data.frame(
-    group = c(rep(1, condition$n), rep(0, condition$n)),
-    y = c(rnorm(condition$n, condition$mean1), rnorm(condition$n, condition$mean0))
-  )
-}
