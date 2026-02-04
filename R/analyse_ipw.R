3a95dcc9784fddc936baf6b515b84a603725b3d1
Starke <paula.starke@med.uni-goettingen.de>
2026-02-04 19:10:09 +0100

    fixed example and package dependecies

--- R/analyse_ipw.R
+++ R/analyse_ipw.R
@@ -19,9 +19,10 @@
 #'
 #' @importFrom stats as.formula
 #' @importFrom estimatr lm_robust
-#' @importFrom dplyr filter lag
+#' @importFrom dplyr filter lag arrange group_by
 #' @importFrom tidyr pivot_longer
 #' @importFrom ipw ipwtm
+#' @importFrom magrittr `%>%`
 #' @details
 #' `alternative` can be "two.sided" for a two sided test of equality of the
 #' summary statistic or "one.sided" for a one sided test testing H0: treatment
@@ -45,23 +46,26 @@
 analyse_ipw <- function(estimand = "tp", level = 0.95, alternative = "two.sided") {
   stopifnot(alternative %in% c("two.sided", "one.sided"))
 
+  # What still remains to be done is
   # documentation
+  # determine if the "alternative" statement can be incorporated
 
   function(condition, dat, fixed_objects = NULL) {
-    dat_long <- tidyr::pivot_longer(dat, y0:y12, names_to = "visit", values_to = "y")
+    k<-condition$k[1]
+    dat_long <- pivot_longer(dat, y0:paste0("y",k), names_to = "visit", values_to = "y")
     dat_long<- dat_long %>%
       mutate(visit = as.numeric(sub('y', '', visit))) %>%
-      mutate(r = ifelse(!is.na(rescue_start)&rescue_start<=visit, 1, 0)) %>% #rescue
-      mutate(r_lag = dplyr::lag(r, default = NA)) %>% #rescue j-1
+      mutate(r = ifelse(!is.na(rescue_start)&rescue_start<=visit, 1L, 0L)) %>% #rescue
+      mutate(r_lag = lag(r, default = NA)) %>% #rescue j-1
       arrange(id, visit) %>% group_by(id) %>%
-      mutate(y_lag = dplyr::lag(y, default = NA)) %>% #HbA1 j-1
+      mutate(y_lag = lag(y, default = NA)) %>% #HbA1 j-1
       mutate(y0 = y[visit == 0]) %>% # y0
       filter(visit!=0) #do not include baseline visits
 
     if (estimand == "tp") {
-      dat_long$exposure<- ifelse(is.na(dat_long$y), 1, 0) #mark missing outcomes (somehow this only works if variable named exactly "exposure")
+      dat_long$exposure<- ifelse(is.na(dat_long$y), 1L, 0L) #indicator for missing outcomes (somehow this only works if variable named exactly "exposure")
 
-      temp <- ipw::ipwtm(
+      temp <- ipwtm(
         exposure = exposure, # indicator for missing data at visit j
         family = "binomial",
         link = "logit",
@@ -71,10 +75,10 @@ analyse_ipw <- function(estimand = "tp", level = 0.95, alternative = "two.sided"
         type = "first",
         data = dat_long
       )
-      model <- estimatr::lm_robust( # OLS with HC2 variance estimator
+      model <- lm_robust( # OLS with HC2 variance estimator
         as.formula(paste0("y ~ trt + y0 + age")),
-        weights = temp$ipw.weights[dat_long$visit==12 & dat_long$exposure==0], # maybe use condition$k[1] instead of "12" if we want to vary the follow-up length
-        data = dat_long[dat_long$visit==12 & dat_long$exposure==0,]
+        weights = temp$ipw.weights[dat_long$visit==k & dat_long$exposure==0],
+        data = dat_long[dat_long$visit==k & dat_long$exposure==0,]
         )
       CI <- confint(model, level=level)
 
@@ -85,9 +89,9 @@ analyse_ipw <- function(estimand = "tp", level = 0.95, alternative = "two.sided"
         ci_upper = CI["trt", 2]
       )
     } else {
-      dat_long$exposure <- ifelse(is.na(dat_long$y) | dat_long$r == 1, 1, 0) #indicator for missing outcomes and rescue medication
+      dat_long$exposure <- ifelse(is.na(dat_long$y) | dat_long$r == 1, 1L, 0L) #indicator for missing outcomes and rescue medication
 
-      temp <- ipw::ipwtm(
+      temp <- ipwtm(
         exposure = exposure, #indicator for missingness or rescue
         family = "binomial",
         link = "logit",
@@ -97,10 +101,10 @@ analyse_ipw <- function(estimand = "tp", level = 0.95, alternative = "two.sided"
         type = "first", #  models are fitted only on observations up to and including the first time point where y is missing, afterwards weights will be constant
         data = dat_long
       )
-      model <- estimatr::lm_robust( # OLS with HC2 variance estimator
+      model <- lm_robust( # OLS with HC2 variance estimator
         as.formula(paste0("y ~ trt + y0 + age")),
-        weights = temp$ipw.weights[dat_long$visit==12 & dat_long$exposure==0],
-        data = dat_long[dat_long$visit==12 & dat_long$exposure==0,]
+        weights = temp$ipw.weights[dat_long$visit==k & dat_long$exposure==0],
+        data = dat_long[dat_long$visit==k & dat_long$exposure==0,]
       )
       CI <- confint(model, level=level)
 
@@ -114,8 +118,4 @@ analyse_ipw <- function(estimand = "tp", level = 0.95, alternative = "two.sided"
   }
 }
 
-# Testing
-#analyse_ipw(estimand = "tp")(assumptions_diabetes_rescue(print = FALSE),
-#   generate_diabetes_rescue(assumptions_diabetes_rescue(print = FALSE)))
-#analyse_ipw(estimand = "hyp")(assumptions_diabetes_rescue(print = FALSE),
-#   generate_diabetes_rescue(assumptions_diabetes_rescue(print = FALSE)))
+
