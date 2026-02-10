af9e589cf83658a1985cc0ac0a0dbdb3100d3e6a
louisejespersen <louise.jespersen@me.com>
2026-02-10 13:29:59 +0100

    Develop lj (#51)
    * finished first working draft
    
    * initial development of demediation analysis (#46)
    
    * initial development of demediation analysis
    
    * tried to fix check bugs
    
    * Namespace fix
    
    * roxygen fix
    
    * fix
    
    * github copilot suggested bug fix
    
    * minor edit
    
    * revert copilot fix
    
    * smaller fix
    
    * fixed example and package dependecies
    
    * Updates for data generation and demediation analysis
    
    * cleaning
    
    * cleaning
    
    * cleaning v2
    
    * updated cleaning
    
    * roxygen adds
    
    * initialized gestimation function
    
    * added true summary statistics to example
    
    * Script for the simulations
    
    * renamed the document from gestimation to gcomputation, gcomputation almost working, minor changes to IPW
    
    * was renamed and uploaded as analyse_gcomputation.R
    
    * minor fixes
    
    * Added diabetes simulation
    
    * working version of analyse_ipw, not yet tested in simulation environment
    
    * updated description etc.
    
    * mmrm
    
    * imports
    
    * adjusted
    
    * description
    
    * Tried to fix
    
    * namespace
    
    * added diabetes_rescue.R file
    
    * adjusted so ipw is simulated too
    
    * some small adjustments to model functions
    
    * Added ipw
    
    * added sample size to true summary statistics and added functions to the script running the simulations
    
    ---------
    
    Co-authored-by: Starke <paula.starke@med.uni-goettingen.de>
    Co-authored-by: Urach Susanne <susanne.urach@ages.at>

--- scripts/diabetes_rescue.R
+++ scripts/diabetes_rescue.R
@@ -1,5 +1,4 @@
-library("CI.RCT.Sim")
-library("SimDesign")
+devtools::load_all()
 
 # -------------------------------------------------------------------
 # Define parameter values and derived quantities
@@ -20,6 +19,9 @@ alpha <- 0.1
 # -------------------------------------------------------------------
 
 my_analyse <- list(
+  ipwtp = analyse_ipw(estimand = "tp"),
+  ipwhyp = analyse_ipw(estimand = "hyp"),
+  dm = analyse_diabetes_demediation(),
   mmrm = analyse_diabetes_rescue_mmrm(ci_level = 1 - alpha)
 )
 
@@ -30,8 +32,10 @@ my_analyse <- list(
 # functions from CI.RCT.Sim / SimDesign
 
 my_summarise <- create_summarise_function(
-
   # bias, SD, coverage etc. for the treatment effect at final visit
+  ipwtp = summarise_estimator(est = coef, real = eff_true),
+  ipwhyp = summarise_estimator(est = coef, real = eff_true),
+  dm = summarise_estimator(est = coef, real = eff_true),
   mmrm = summarise_estimator(
     est   = coef,
     real  = eff_true,
@@ -39,7 +43,6 @@ my_summarise <- create_summarise_function(
     upper = ci_upper,
     null  = 0
   ),
-
   # additional custom summary: mean CI width
   mmrm = function(condition, results, fixed_objects = NULL) {
     data.frame(
@@ -65,12 +68,14 @@ results <- runSimulation(
 # -------------------------------------------------------------------
 
 results |>
-  subset(
-    select = c(
-      names(sim_parameters),
-      "mmrm.bias",
-      "mmrm.sd_est",
-      "mmrm.coverage",
-      "mmrm.1.mean_ci_width"
-    )
-  )
+  subset(select = c(
+    # names(sim_parameters),
+    "ipw.bias",
+    "ipw.sd_est",
+    "dm.bias",
+    "dm.sd_est",
+    "mmrm.bias",
+    "mmrm.sd_est",
+    "mmrm.coverage",
+    "mmrm.1.mean_ci_width"
+  ))
