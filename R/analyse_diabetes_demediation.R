bc022080e6ef1c2ecd6ebb88cbdc1c3f7ed705b9
louisejespersen <louise.jespersen@me.com>
2026-02-16 13:54:08 +0100

    Initialised firth correction, and specified multiple imputation by group

--- R/analyse_diabetes_demediation.R
+++ R/analyse_diabetes_demediation.R
@@ -5,10 +5,12 @@
 #' @return an analyse function that can be used in runSimulation
 #' @export
 #'
-#' @importFrom stats lm glm as.formula binomial coef predict var
+#' @importFrom stats lm glm as.formula binomial coef predict var quantile
+#' @importFrom mice mice make.method make.predictorMatrix complete rbind pool.scalar
+#' @importFrom logistf logistf
 #'
 #' @examples
-#' setting <- assumptions_diabetes_rescue()[1, ]
+#' setting <- assumptions_diabetes_rescue()[1, ] |> true_summary_statistics_diabetes_rescue()
 #' dat <- generate_diabetes_rescue(setting)
 #' analyse_diabetes_demediation()(setting, dat)
 #'
@@ -16,7 +18,17 @@ analyse_diabetes_demediation <- function(X) {
   function(condition, dat, fixed_objects = NULL) {
     # Convert the logical of receiving rescue at any point in to a longitudinal measurement in wide format
     dat <- dat |> dplyr::mutate(rescue_start = ifelse(is.na(rescue_start), condition$k + 2, rescue_start))
-    dats <- mice::mice(dat, m = 5, print = FALSE)
+
+    dat1 <- dat |> dplyr::filter(trt == 1)
+    pred1 <- mice::make.predictorMatrix(dat1)
+    pred1[, c("id", "rescue_start", "trt")] <- 0
+    dat0 <- dat |> dplyr::filter(trt == 0)
+    pred0 <- mice::make.predictorMatrix(dat0)
+    pred0[, c("id", "rescue_start", "trt")] <- 0
+    dats <- mice::rbind(
+      mice::mice(dat1, m = 5, print = FALSE, predictorMatrix = pred1),
+      mice::mice(dat0, m = 5, print = FALSE, predictorMatrix = pred0)
+    )
 
     analysis <- function(dat, indicator) {
       dat_comp <- dat[indicator, ] |> dplyr::mutate(
@@ -30,10 +42,16 @@ analyse_diabetes_demediation <- function(X) {
       dat_comp[, "j11"] <- dat_comp$yc12
       for (k in 1:11) {
         # Fit a model to predict the probability of receiving rescue medication at visit 12 - k
+        # browser()
         # mod <- glm(
         #   as.formula(paste0("R", 12 - k, "~ trt + age + y0", paste0("+ yc", 1:(12 - k), collapse = " "))),
         #   data = dat_comp,
-        #   family = binomial(link = "probit")
+        #   family = binomial(link = "probit"),
+        #   method = "brglmFit"
+        # )
+        # mod <- logistf(
+        #   as.formula(paste0("R", 12 - k, "~ trt + age + y0", paste0("+ yc", 1:(12 - k), collapse = " "))),
+        #   data = dat_comp
         # )
         # dat_comp[, paste0("pred_R", 12 - k)] <- predict(mod, type = "response")
 
@@ -68,27 +86,35 @@ analyse_diabetes_demediation <- function(X) {
 
 
       final.model <- lm(j0 ~ trt + y0, data = dat_comp)
-      # browser(expr = abs(final.model$coefficients["trt"]) > 1)
+      # browser()
       c(
         p = summary(final.model)$coefficients["trt", "Pr(>|t|)"],
-        coef = coef(final.model)["trt"]
+        coef = coef(final.model)["trt"],
+        se = summary(final.model)$coefficients["trt", "Std. Error"]
       )
     }
     effect <- rep(NA, dats$m)
     effect.var <- rep(NA, dats$m)
+    cil <- rep(NA, dats$m)
+    ciu <- rep(NA, dats$m)
+    # res <- vector("list", dats$m)
     # browser()
     for (i in 1:dats$m) {
       dat <- mice::complete(dats, i)
       res <- boot::boot(dat, analysis, R = 500)
       effect[i] <- res$t0[2]
       effect.var[i] <- var(res$t[, 2])
+      cil[i] <- quantile(res$t[, 2], probs = c(0.025))
+      ciu[i] <- quantile(res$t[, 2], probs = c(0.975))
     }
     end_res <- mice::pool.scalar(effect, effect.var)
 
     list(
-      # effect,
-      # effect.var,
-      # coefs = end_res$qhat,
+      effect,
+      effect.var,
+      cil,
+      ciu,
+      coefs = end_res$qhat,
       coef = end_res$qbar,
       sd = sqrt(end_res$t)
     )
