45ddf982bf1b5633f1fb3e44e1667b3ca611c744
louisejespersen <louise.jespersen@me.com>
2026-02-19 10:41:23 +0100

    Implemented a list with parameters for missingness in the assumptions. New parameter nfix, if one wants to skip the sample size calculation

--- R/generate_diabetes_rescue.R
+++ R/generate_diabetes_rescue.R
@@ -58,7 +58,10 @@ generate_diabetes_rescue <- function(condition, fixed_objects = NULL) {
   sigma <- condition$sd_bl
   sigma_adj <- sqrt(sigma^2 * (1 - rho^2))
 
-  n <- 2 * round(((qnorm(1 - alpha / 2) + qnorm(power))^2) * sigma_adj^2 * 2 / (delta_true^2))
+  ifelse(is.null(condition$nfix),
+    n <- 2 * round(((qnorm(1 - alpha / 2) + qnorm(power))^2) * sigma_adj^2 * 2 / (delta_true^2)),
+    n <- condition$nfix
+  )
   if (condition$hyp == 0) {
     eff <- rep(0, condition$k + 1)
   }
@@ -118,9 +121,9 @@ generate_diabetes_rescue <- function(condition, fixed_objects = NULL) {
   }
 
   # Implement dropout
-  p_miss <- plogis(condition$miss_0 + (Y - 10) * condition$miss_y +
-    (age - condition$mean_age) * condition$miss_age +
-    rescue * condition$miss_resc) # actual prob. to drop out
+  p_miss <- plogis(condition$miss[[1]][1] + (Y - 10) * condition$miss[[1]][2] +
+    (age - condition$mean_age) * condition$miss[[1]][3] +
+    rescue * condition$miss[[1]][4]) # actual prob. to drop out
   p_miss[, 1] <- 0 # we assume complete data at baseline
 
   wd <- matrix(rbinom((condition$k + 1) * n, size = 1, prob = p_miss), nrow = n)
@@ -134,7 +137,7 @@ generate_diabetes_rescue <- function(condition, fixed_objects = NULL) {
     }
   }
 
-  out <- data.frame(id, trt, age, Y, rescue_start, rescue[, 2:condition$k]*1)
+  out <- data.frame(id, trt, age, Y, rescue_start, rescue[, 2:condition$k] * 1)
   names(out) <- c("id", "trt", "age", paste("y", visit, sep = ""), "rescue_start", paste("R", visit[-1][1:condition$k - 1], sep = ""))
   out
 }
@@ -168,15 +171,16 @@ assumptions_diabetes_rescue <- function(print = interactive()) {
   delta       = -c(1,0.5),                # Maximal treatment effect
   lambda      = log(2)/2,                 # Rate of increasing treatment effect
   delta_resc  = -0.75,                    # Maximal effect of rescue medication
-  lambda_resc = 1,                        # Rate of increasing effect of rescue medication
+  lambda_resc = log(2),                        # Rate of increasing effect of rescue medication
   resc_0      = qlogis(c(0.05,0.02)),     # probability for rescue medication
   resc_y      = log(c(3,150)),            # strong effect due to high hba1c
   resc_age    = -log(1.01),               # weaker age effect than for dropout
-  miss_0      = qlogis(c(0.02,0.5,0.04)), # probability for missing data
-  miss_y      = log(c(3,1,150)),          # moderate effect due to high hba1c
-  miss_age    = c(log(1.02),0),           # older patients drop out more easily, let's say, stronger age effect than for rescue
-  miss_resc   = c(log(1.5),0)             # notable effect due to rescue medication, increase to large value to have positivity violation, like 5 or 10
-  )
+  miss        = list(
+  c(qlogis(0.02), log(3),log(1.02),log(1.5)), # probability for missing data in the core scenario
+  c(-100000,0,0,0),                                # probability for missing data in the scenario with no dropout
+  c(qlogis(0.04),log(150),log(1.02),log(1.5))# probability for missing data in the scenario with stronger dropout
+  )) |>
+  merge(data.frame(hyp=c(0,1)), by=NULL)
 "
 
 
@@ -184,12 +188,11 @@ assumptions_diabetes_rescue <- function(print = interactive()) {
     cat(skel)
   }
 
-  r <- invisible(
+  invisible(
     skel |>
       str2expression() |>
       eval()
   )
-  rbind(r |> dplyr::mutate(hyp = 0), r |> dplyr::mutate(hyp = 1))
 }
 
 #' Calculate true summary statistics for scenarios with delayed treatment effect
@@ -218,7 +221,6 @@ true_summary_statistics_diabetes_rescue <- function(Design, cutoff_stats = 10, f
   #   res <- data.frame(
   #     eff_true <- condition$delta / 2 * (1 - exp(-condition$lambda * condition$k))
   #   )
-  #
   #   res
   # }
   #
@@ -233,8 +235,10 @@ true_summary_statistics_diabetes_rescue <- function(Design, cutoff_stats = 10, f
   alpha <- 0.05
   power <- 0.8
 
-  Design$n <- 2 * round(((qnorm(1 - alpha / 2) + qnorm(power))^2) * Design$sd_bl^2 * (1 - Design$rho^2) * 2 / (Design$eff_true^2))
-
+  Design$n <- ifelse(is.null(Design$nfix),
+    2 * round(((qnorm(1 - alpha / 2) + qnorm(power))^2) * Design$sd_bl^2 * (1 - Design$rho^2) * 2 / (Design$eff_true^2)),
+    Design$nfix
+  )
 
   Design$eff_true <- ifelse(Design$hyp == 1, Design$eff_true, 0)
   Design
