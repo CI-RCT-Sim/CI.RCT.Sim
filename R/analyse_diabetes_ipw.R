ff3186952237025530c995cea4f49136192c4eaa
Starke <paula.starke@med.uni-goettingen.de>
2026-03-02 11:33:04 +0100

    Adressed all comments from review:
    changed starts_with("y") similar to g-computation to include new variable "R"
    used "R" instead of custom "rescue" variable
    at baseline set R=0 and at last timepoint set R=lag(R) (MAYBE: code it like this already in data generation?)
    changed so that no exlusion based on exposure == 0

--- R/analyse_diabetes_ipw.R
+++ R/analyse_diabetes_ipw.R
@@ -38,22 +38,22 @@ analyse_diabetes_ipw <- function(estimand = "hyp") {
     k <- condition$k # number of last visit
 
     # reformate dat to long format with one Hba1c column, one outcome column 'y' (change of Hba1c) and a time variable 'visit'
-    dat_long <- pivot_longer(
-      dat,
-      starts_with("y"),
-      names_to = "visit",
-      values_to = "hba1c"
+    dat_long <- tidyr::pivot_longer(dat,
+                                    cols = matches("^[yR]\\d+$"),
+                                    names_to = c(".value", "visit"),
+                                    names_pattern = "([yR])(\\d+)"
     ) %>%
       mutate(
+        hba1c = y,
         visit = as.numeric(sub("y", "", visit)),
-        rescue = ifelse(!is.na(rescue_start) & rescue_start <= visit, 1, 0), # new variable for rescue at visit j
-        rescue_lag = lag(rescue, default = NA)
-      ) %>% # rescue at visit j-1
+        R = ifelse(visit == 0, 0, ifelse(visit == condition$k, dplyr::lag(R), R)), # set rescue at baseline to 0
+        R_lag = dplyr::lag(R, default = NA)
+      ) %>%
       arrange(id, visit) %>%
-      group_by(id) %>%
+      group_by(id) %>% # make sure table is grouped by id and ordered by visit
       mutate(
         hba1c_0 = hba1c[visit == 0], # HbA1 at baseline
-        hba1c_lag = lag(hba1c, default = NA), # HbA1 at visit j-1
+        hba1c_lag = dplyr::lag(hba1c, default = NA), # HbA1 at visit j-1
         y = hba1c - hba1c_0
       ) %>% # HbA1c change
       filter(visit != 0) # do not include baseline visits
@@ -65,7 +65,7 @@ analyse_diabetes_ipw <- function(estimand = "hyp") {
         exposure = exposure, # indicator for missing data at visit j
         family = "binomial",
         link = "logit",
-        denominator = ~ trt + age + hba1c_lag + rescue_lag,
+        denominator = ~ trt + age + hba1c_lag + R_lag,
         id = id,
         timevar = visit,
         type = "first",
@@ -73,8 +73,8 @@ analyse_diabetes_ipw <- function(estimand = "hyp") {
       )
       model <- lm_robust( # OLS with HC2 variance estimator
         as.formula(paste0("y ~ trt + hba1c_0 + age")),
-        weights = temp$ipw.weights[dat_long$visit == k & dat_long$exposure == 0],
-        data = dat_long[dat_long$visit == k & dat_long$exposure == 0, ]
+        weights = temp$ipw.weights[dat_long$visit == k],
+        data = dat_long[dat_long$visit == k,]
       )
 
       list(
@@ -82,7 +82,7 @@ analyse_diabetes_ipw <- function(estimand = "hyp") {
         sd = summary(model)$coefficients["trt", "Std. Error"]
       )
     } else if (estimand == "hyp") {
-      dat_long$exposure <- ifelse(is.na(dat_long$y) | dat_long$rescue == 1, 1L, 0L) # indicator for missing outcomes and rescue medication
+      dat_long$exposure <- ifelse(is.na(dat_long$y) | dat_long$R == 1, 1L, 0L) # indicator for missing outcomes and rescue medication
 
       temp <- ipw::ipwtm(
         exposure = exposure, # indicator for missingness or rescue
@@ -96,8 +96,8 @@ analyse_diabetes_ipw <- function(estimand = "hyp") {
       )
       model <- estimatr::lm_robust( # OLS with HC2 variance estimator
         as.formula(paste0("y ~ trt + hba1c_0 + age")),
-        weights = temp$ipw.weights[dat_long$visit == k & dat_long$exposure == 0],
-        data = dat_long[dat_long$visit == k & dat_long$exposure == 0, ]
+        weights = temp$ipw.weights[dat_long$visit == k],
+        data = dat_long[dat_long$visit == k,]
       )
 
       list(
