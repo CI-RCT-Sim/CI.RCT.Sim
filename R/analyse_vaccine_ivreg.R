58434ee0f2ecd3ceca522af4a0a5259a6f4f6e7c
Fellinger Tobias <tobias.fellinger@ages.at>
2026-03-05 13:19:17 +0100

    IV reg function for vaccine scenario

--- R/analyse_vaccine_ivreg.R
+++ R/analyse_vaccine_ivreg.R
@@ -11,13 +11,13 @@
 #' @export
 #'
 #' @importFrom ivreg ivreg
+#' @importFrom emmeans emmeans regrid
 #'
 #' @examples
 #' Design <- vaccine_scenario() |>
 #'   vaccine_scenario_set_gamma_0() |>
 #'   vaccine_scenario_set_true_eff() |>
 #'   vaccine_scenario_set_samplesize()
-#' Design
 #'
 #' dat <- generate_vaccine(Design[1,])
 #' my_analyse <- analyse_vaccine_ivreg(ci_level=0.95)
@@ -25,18 +25,62 @@
 analyse_vaccine_ivreg <- function(ci_level=0.95, VE_margin=0.3){
   function(condition, dat, fixed_objects = NULL){
 
-    dat <- dat |>
+    dat1 <- dat |>
       within({
-        T <- ifelse(((trt==1) & (C==1)), 1L, 0L)
+        T <- factor(ifelse(((trt==1) & (C==1)), 1L, 0L), levels=c("1", "0"))
+        V <- factor(V)
+        trt <- factor(trt)
+        evt <- as.integer(evt)
       })
 
-    ivreg::ivreg(evt ~ T + V | trt, data=dat)
+    # lm drops (multi-)colinear values but but we need to know which columns are
+    # included in the second stage of the estimation therefore we build our
+    # formulas and variable names here
+    formula_stage1x <- ~ trt
+    formula_stage1y <- ~ T
+    vars_stage1 <- c("T0")
+    vars_stage2 <- c("stage1_T")
+    formula_stage2 <- evt ~ stage1_1 + stage1_T
+
+    if(length(unique(dat$V)) > 1){
+      formula_stage1x <- update(formula_stage1x, ~ . + V)
+      formula_stage1y <- update(formula_stage1y, ~ . + V)
+      formula_stage2  <- update(formula_stage2 , ~ . + stage1_V)
+      vars_stage1 <- c(vars_stage1, "V1")
+      vars_stage2 <- c(vars_stage2, "stage1_V")
+    }
+
+    if(length(unique(dat$W)) > 1){
+      formula_stage1x <- update(formula_stage1x, ~ . + W)
+      formula_stage1y <- update(formula_stage1y, ~ . + W)
+      formula_stage2  <- update(formula_stage2 , ~ . + stage1_W)
+      vars_stage1 <- c(vars_stage1, "V1")
+      vars_stage2 <- c(vars_stage2, "stage1_W")
+    }
+
+    vars_stage1 <- c(vars_stage1, "(Intercept)")
+    vars_stage2 <- c(vars_stage2, "stage1_1")
+    formula_stage2  <- update(formula_stage2 , ~ . -1)
+
+    x_stage1 <- model.matrix(formula_stage1x, data=dat1)
+    y_stage1 <- model.matrix(formula_stage1y, data=dat1)
+
+    lm_stage1 <- lm.fit(x=x_stage1, y=y_stage1)
+    dat1[, vars_stage2] <- lm_stage1$fitted.values[, vars_stage1]
+
+    stage2 <- lm(formula_stage2, data=dat1)
+
+    # marginalize over other variables
+    emm <- emmeans(stage2, ~ stage1_T, at=list(stage1_T=c(0,1)))
+    lemm <- regrid(emm, "log")
+    res <- pairs(lemm, type = "response")
+    ci <- confint(res)
 
     list(
-      p = p_val,
-      VE = ci[1,"Estimate"],
-      VE_lower = ci[1,"upr"],
-      VE_upper = ci[1,"lwr"]
+      p = summary(res)$p.value,
+      VE = 1-ci$ratio,
+      VE_lower = 1-ci$upper.CL,
+      VE_upper = 1-ci$lower.CL
     )
   }
 }
