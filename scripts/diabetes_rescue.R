c510cff4004df40cb24f36dfcba693ef85e55077
Louise J. <louise@ims-node2.cemsiis.meduniwien.ac.at>
2026-03-06 11:09:21 +0100

    Updated accordingly to function updates

--- scripts/diabetes_rescue.R
+++ scripts/diabetes_rescue.R
@@ -20,14 +20,14 @@ alpha <- 0.1
 
 my_analyse <- list(
   ## Treatment policy estimands
-  ipwtp = analyse_ipw(estimand = "tp"),
-  mmrmtp = analyse_diabetes_rescue_mmrm(ci_level = 1 - alpha, strategy = "treatment_policy"),
+  ipwtp = analyse_diabetes_ipw(estimand = "tp"),
+  mmrmtp = analyse_diabetes_rescue_mmrm(strategy = "treatment_policy"),
   mitp = analyse_diabetes_rescue_mi(estimand = "treatment_policy"),
   ## Hypothetical estimands
-  ipwhyp = analyse_ipw(estimand = "hyp"),
+  ipwhyp = analyse_diabetes_ipw(estimand = "hyp"),
   dm = analyse_diabetes_demediation(),
   gcom = analyse_diabetes_gcomputation(),
-  mmrmhyp = analyse_diabetes_rescue_mmrm(ci_level = 1 - alpha, strategy = "hypothetical"),
+  mmrmhyp = analyse_diabetes_rescue_mmrm(strategy = "hypothetical"),
   mihyp = analyse_diabetes_rescue_mi(estimand = "hypothetical")
 )
 
