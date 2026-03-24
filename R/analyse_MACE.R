2c91f0b53b458fca8c3837e4613d163d22c55928
Fellinger Tobias <tobias.fellinger@ages.at>
2026-03-24 16:31:09 +0100

    replaced custom summarise function with generic one
    * removed summarise_func_mace
    * added output of p-value to analyse_mace_... function
    * replaced summarise_func_mace with summarise_estimator and summarise_test in mace_script

--- R/analyse_MACE.R
+++ R/analyse_MACE.R
@@ -14,11 +14,11 @@
 #' toy_data_trunc <- censor_buffer_window(toy_data,buffer)
 censor_buffer_window <- function(data,buffer) {
   disc_not_withdraw <- data$event_disc==1 & data$withdraw==0
-  
+
   data[disc_not_withdraw,"event_mace"] <- ifelse(data[disc_not_withdraw,"t_mace"]-data[disc_not_withdraw,"t_disc"]<buffer,
                                                  data[disc_not_withdraw,"event_mace"],
                                                  0)
-  
+
   data[disc_not_withdraw,"t_mace"] <- ifelse(data[disc_not_withdraw,"t_mace"]-data[disc_not_withdraw,"t_disc"]<buffer,
                                                  data[disc_not_withdraw,"t_mace"],
                                                  pmin(1,data[disc_not_withdraw,"t_disc"]+buffer))
@@ -29,10 +29,10 @@ censor_buffer_window <- function(data,buffer) {
 
 #' Cox model without covariates
 #' @param data data in wide format, output of censor_buffer window
-#' 
+#'
 #' @importFrom survival coxph
-#' 
-#' @return a coxph fit 
+#'
+#' @return a coxph fit
 #' @export
 #'
 #' @examples
@@ -50,8 +50,8 @@ cox_model_nocov <- function(data) {
 #' @param data data in wide format, output of censor_buffer window
 #'
 #' @importFrom survival coxph
-#' 
-#' @return a coxph fit 
+#'
+#' @return a coxph fit
 #' @export
 #'
 #' @examples
@@ -86,14 +86,14 @@ data_process_start_stop <- function(data) {
   disc_not_withdraw <- data$event_disc==1 & data$withdraw==0
   data$disc=0
   data2 <- data[disc_not_withdraw,]
-  
+
   data$t_mace_start <- 0
   data$t_mace_stop <- ifelse(data$event_disc==1,data$t_disc,ifelse(data$event_mace==1,data$t_mace,1))
-  
+
   data2$t_mace_start <- data2$t_disc
   data2$t_mace_stop <- data2$t_mace
   data2$disc <- 1
-  
+
   return(rbind(data,data2)|>arrange(ID,t_mace_start))
 }
 
@@ -101,7 +101,7 @@ data_process_start_stop <- function(data) {
 #' Cox model with IPW weights with covariates
 #'
 #' @param data simulated data with long format, output of data_process_start_stop function
-#' 
+#'
 #' @importFrom survival coxph
 #' @importFrom stats glm
 #'
@@ -116,12 +116,12 @@ data_process_start_stop <- function(data) {
 #' toy_data_trunc_long <- data_process_start_stop(toy_data_trunc)
 #' cox_model_ipw(toy_data_trunc_long)
 cox_model_ipw <- function(data) {
-  
+
   dat_withdraw <- data |> filter(event_disc==1,!duplicated(ID))
   fit_withdraw_0 <- glm(withdraw~X+Z,data=dat_withdraw|>filter(A==0),family = binomial(link = "logit"))
   fit_withdraw_1 <- glm(withdraw~X+Z,data=dat_withdraw|>filter(A==1),family = binomial(link = "logit"))
-  
-  
+
+
   p0 <- predict(fit_withdraw_0, newdata = data, type = "response")
   p1 <- predict(fit_withdraw_1, newdata = data, type = "response")
   lp <- c()
@@ -132,14 +132,14 @@ cox_model_ipw <- function(data) {
       }else {
         lp <- c(lp,1-p1[i])
       }
-      
+
     } else {
       lp <- c(lp,1)
-    } 
-  } 
+    }
+  }
   data$IPW <- 1/lp
   (coxph(Surv(t_mace_start,t_mace_stop,event_mace)~A+X+Z,data=data,id=ID,weights=IPW))
-  
+
 }
 
 
@@ -148,10 +148,10 @@ cox_model_ipw <- function(data) {
 #' Cox model with IPW weights without covariates
 #'
 #' @param data simulated data with long format, output of data_process_start_stop function
-#' 
+#'
 #' @importFrom survival coxph
 #' @importFrom stats glm
-#' 
+#'
 #' @return a coxph fit
 #' @export
 #'
@@ -163,12 +163,12 @@ cox_model_ipw <- function(data) {
 #' toy_data_trunc_long <- data_process_start_stop(toy_data_trunc)
 #' cox_model_ipw_nocov(toy_data_trunc_long)
 cox_model_ipw_nocov <- function(data) {
-  
+
   dat_withdraw <- data |> filter(event_disc==1,!duplicated(ID))
   fit_withdraw_0 <- glm(withdraw~X+Z,data=dat_withdraw|>filter(A==0),family = binomial(link = "logit"))
   fit_withdraw_1 <- glm(withdraw~X+Z,data=dat_withdraw|>filter(A==1),family = binomial(link = "logit"))
-  
-  
+
+
   p0 <- predict(fit_withdraw_0, newdata = data, type = "response")
   p1 <- predict(fit_withdraw_1, newdata = data, type = "response")
   lp <- c()
@@ -179,14 +179,14 @@ cox_model_ipw_nocov <- function(data) {
       }else {
         lp <- c(lp,1-p1[i])
       }
-      
+
     } else {
       lp <- c(lp,1)
-    } 
-  } 
+    }
+  }
   data$IPW <- 1/lp
   (coxph(Surv(t_mace_start,t_mace_stop,event_mace)~A,data=data,id=ID,weights=IPW))
-  
+
 }
 
 
@@ -205,24 +205,27 @@ cox_model_ipw_nocov <- function(data) {
 #' CI.l CI.u = lower and upper CI of HR
 #' @export
 #'
+#' @importFrom stats pchisq
+#'
 analyse_mace_cox_nocov <- function(condition, dat, fixed_objects = NULL) {
   assumed_window <- condition$assumed_window
-  
+
   trunc_data <- censor_buffer_window(dat,assumed_window)
   cox_nocov <- cox_model_nocov(trunc_data)
-  
+
   cox_nocov.trt <- coef(cox_nocov)["A"]
   cox_nocov.trt.se <- summary(cox_nocov)$coefficient[1,"se(coef)"]
   cox_nocov.HR <- exp(cox_nocov.trt)
   cox_nocov.HR.CI.l <- summary(cox_nocov)$conf.int[1,3]
   cox_nocov.HR.CI.u <- summary(cox_nocov)$conf.int[1,4]
-  
+
   list(
        coef=cox_nocov.trt,
        se=cox_nocov.trt.se,
        HR=cox_nocov.HR,
        CI.l=cox_nocov.HR.CI.l,
-       CI.u=cox_nocov.HR.CI.u
+       CI.u=cox_nocov.HR.CI.u,
+       p=1-pchisq((cox_nocov.trt^2)/(cox_nocov.trt.se^2), 1)
        )
 }
 
@@ -240,24 +243,27 @@ analyse_mace_cox_nocov <- function(condition, dat, fixed_objects = NULL) {
 #' CI.l CI.u = lower and upper CI of HR
 #' @export
 #'
+#' @importFrom stats pchisq
+#'
 analyse_mace_cox_cov <- function(condition, dat, fixed_objects = NULL) {
   assumed_window <- condition$assumed_window
-  
+
   trunc_data <- censor_buffer_window(dat,assumed_window)
   cox_cov <- cox_model_cov(trunc_data)
-  
+
   cox_cov.trt <- coef(cox_cov)["A"]
   cox_cov.trt.se <- summary(cox_cov)$coefficient[1,"se(coef)"]
   cox_cov.HR <- exp(cox_cov.trt)
   cox_cov.HR.CI.l <- summary(cox_cov)$conf.int[1,3]
   cox_cov.HR.CI.u <- summary(cox_cov)$conf.int[1,4]
-  
+
   list(
     coef=cox_cov.trt,
     se=cox_cov.trt.se,
     HR=cox_cov.HR,
     CI.l=cox_cov.HR.CI.l,
-    CI.u=cox_cov.HR.CI.u
+    CI.u=cox_cov.HR.CI.u,
+    p=1-pchisq((cox_cov.trt^2)/(cox_cov.trt.se^2), 1)
   )
 }
 
@@ -276,26 +282,29 @@ analyse_mace_cox_cov <- function(condition, dat, fixed_objects = NULL) {
 #' CI.l CI.u = lower and upper CI of HR
 #' @export
 #'
+#' @importFrom stats pchisq
+#'
 analyse_mace_ipw_nocov <- function(condition, dat, fixed_objects = NULL) {
   assumed_window <- condition$assumed_window
-  
+
   trunc_data <- censor_buffer_window(dat,assumed_window)
   long_trunc_data <- data_process_start_stop(trunc_data)
 
   ipw_nocov <- cox_model_ipw_nocov(long_trunc_data)
-  
+
   ipw_nocov.trt <- coef(ipw_nocov)["A"]
   ipw_nocov.trt.se <- summary(ipw_nocov)$coefficient[1,"robust se"]
   ipw_nocov.HR <- exp(ipw_nocov.trt)
   ipw_nocov.HR.CI.l <- summary(ipw_nocov)$conf.int[1,3]
   ipw_nocov.HR.CI.u <- summary(ipw_nocov)$conf.int[1,4]
-  
+
   list(
     coef=ipw_nocov.trt,
     se=ipw_nocov.trt.se,
     HR=ipw_nocov.HR,
     CI.l=ipw_nocov.HR.CI.l,
-    CI.u=ipw_nocov.HR.CI.u
+    CI.u=ipw_nocov.HR.CI.u,
+    p=1-pchisq((ipw_nocov.trt^2)/(ipw_nocov.trt.se^2), 1)
   )
 }
 
@@ -313,62 +322,83 @@ analyse_mace_ipw_nocov <- function(condition, dat, fixed_objects = NULL) {
 #' CI.l CI.u = lower and upper CI of HR
 #' @export
 #'
+#' @importFrom stats pchisq
+#'
 analyse_mace_ipw_cov <- function(condition, dat, fixed_objects = NULL) {
   assumed_window <- condition$assumed_window
-  
+
   trunc_data <- censor_buffer_window(dat,assumed_window)
   long_trunc_data <- data_process_start_stop(trunc_data)
   ipw_cov <- cox_model_ipw(long_trunc_data)
-  
+
   ipw_cov.trt <- coef(ipw_cov)["A"]
   ipw_cov.trt.se <- summary(ipw_cov)$coefficient[1,"robust se"]
   ipw_cov.HR <- exp(ipw_cov.trt)
   ipw_cov.HR.CI.l <- summary(ipw_cov)$conf.int[1,3]
   ipw_cov.HR.CI.u <- summary(ipw_cov)$conf.int[1,4]
-  
+
   list(
     coef=ipw_cov.trt,
     se=ipw_cov.trt.se,
     HR=ipw_cov.HR,
     CI.l=ipw_cov.HR.CI.l,
-    CI.u=ipw_cov.HR.CI.u
+    CI.u=ipw_cov.HR.CI.u,
+    p=1-pchisq((ipw_cov.trt^2)/(ipw_cov.trt.se^2), 1)
   )
 }
 
 
 
 
-#' Generic Summarise function 
-#'
-#' @param condition SimDesign element
-#' @param results SimDesign element
-#' @param fixed_objects SimDesign element
-#'
-#' @return
-#' bias = bias on the treatment effect
-#' mean_se = mean estimated standard error
-#' emp_se = empirical se
-#' mean_HR = mean estimated hazard ratio
-#' mean_HR_CI_width = mean width of 95% confidence interval of HR
-#' power = power/type 1 error using a Wald test
-#' @export
-#'
-summarise_func_mace <- function(condition, results, fixed_objects) {
-  true_trt <- as.numeric(condition$true_trt)
-  
-  with(results, {    
-    
-    list_results <- data.frame(   
-      
-      bias = mean(coef-true_trt),
-      mean_se = mean(se),
-      emp_se = sd(coef),
-      mean_HR = mean(HR),
-      mean_HR_CI_width = mean(CI.u-CI.l),
-      CR = mean(((true_trt) > log(CI.l)) & ((true_trt) < log(CI.u))),
-      power = mean((coef^2)/(se^2)>qchisq(0.95,1))
-      
-    )
-    return((list_results))
-  })
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- scripts/mace_script.R
+++ scripts/mace_script.R
@@ -10,7 +10,7 @@ sim_parameters <- mace_scenario() |> true_trt_mace()
 # Constants for simulation
 # -------------------------------------------------------------------
 
-N_sim <- 1000
+N_sim <- 100
 
 # -------------------------------------------------------------------
 # List of analysis functions
@@ -20,16 +20,25 @@ my_analyse <- list(
   cox_nocov=analyse_mace_cox_nocov,
   cox_cov=analyse_mace_cox_cov,
   ipw_nocov=analyse_mace_ipw_nocov,
-  ipw_cov=analyse_mace_ipw_cov 
+  ipw_cov=analyse_mace_ipw_cov
 )
 
 
 my_summarise <- create_summarise_function(
-  cox_nocov=summarise_func_mace,
-  cox_cov=summarise_func_mace,
-  ipw_nocov=summarise_func_mace,
-  ipw_cov=summarise_func_mace 
+  cox_nocov=summarise_estimator(est=coef, real=true_trt, est_sd=se, name="est"),
+  cox_cov  =summarise_estimator(est=coef, real=true_trt, est_sd=se, name="est"),
+  ipw_nocov=summarise_estimator(est=coef, real=true_trt, est_sd=se, name="est"),
+  ipw_cov  =summarise_estimator(est=coef, real=true_trt, est_sd=se, name="est"),
+  cox_nocov=summarise_estimator(est=HR, real=exp(true_trt), lower=CI.l, upper=CI.u, null=1, name="HR"),
+  cox_cov  =summarise_estimator(est=HR, real=exp(true_trt), lower=CI.l, upper=CI.u, null=1, name="HR"),
+  ipw_nocov=summarise_estimator(est=HR, real=exp(true_trt), lower=CI.l, upper=CI.u, null=1, name="HR"),
+  ipw_cov  =summarise_estimator(est=HR, real=exp(true_trt), lower=CI.l, upper=CI.u, null=1, name="HR"),
+  cox_nocov=summarise_test(alpha=0.05, name="test"),
+  cox_cov  =summarise_test(alpha=0.05, name="test"),
+  ipw_nocov=summarise_test(alpha=0.05, name="test"),
+  ipw_cov  =summarise_test(alpha=0.05, name="test")
 )
+
 # -------------------------------------------------------------------
 # Run the simulations
 # -------------------------------------------------------------------
@@ -45,4 +54,17 @@ results <- runSimulation(
 # -------------------------------------------------------------------
 # Inspect results
 # -------------------------------------------------------------------
-results[,grep("power",colnames(results))]
+
+methods <- c("cox_nocov", "cox_cov", "ipw_nocov", "ipw_cov")
+
+# estimators HR scale
+results[, paste(methods, "HR", "bias", sep=".")]
+results[, paste(methods, "HR", "width", sep=".")]
+
+# estimator log-HR scale
+results[, paste(methods, "est", "bias", sep=".")]
+results[, paste(methods, "est", "sd_est", sep=".")]
+
+# test decision from CI and test
+results[, paste(methods, "test", "rejection_0.05", sep=".")]
+1-results[, paste(methods, "HR", "null_cover", sep=".")]
