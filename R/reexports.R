c723fa8e9c9526777bd125523681bbfe0689fca1
louisejespersen <louise.jespersen@me.com>
2026-03-27 13:35:02 +0100

    Try to fix bug

--- R/reexports.R
+++ R/reexports.R
@@ -7,6 +7,10 @@ SimNPH::create_summarise_function
 SimNPH::summarise_estimator
 
 #' @export
+#' @importFrom SimNPH analyse_describe
+SimNPH::analyse_describe
+
+#' @export
 #' @importFrom SimNPH summarise_describe
 SimNPH::summarise_describe
 
