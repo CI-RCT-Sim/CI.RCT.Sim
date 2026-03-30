a48df918f5a89bcd5f51e862a2da52ec15cf248c
Fellinger Tobias <tobias.fellinger@ages.at>
2026-03-30 09:51:38 +0200

    refactored changes to vaccine_scenario, related functions and dok

--- tests/testthat/test-generate_vaccine.R
+++ tests/testthat/test-generate_vaccine.R
@@ -1,5 +1,6 @@
 test_that("setting sample size works", {
   Design1 <- vaccine_scenario() |>
+    vaccine_scenario_set_beta_A1_relative() |>
     vaccine_scenario_set_gamma_0() |>
     vaccine_scenario_set_true_eff() |>
     vaccine_scenario_set_samplesize()
@@ -9,6 +10,7 @@ test_that("setting sample size works", {
   expect_true(all(is.finite(Design1$n_trt)))
 
   Design2 <- vaccine_scenario() |>
+    vaccine_scenario_set_beta_A1_relative() |>
     vaccine_scenario_set_gamma_0() |>
     vaccine_scenario_set_true_eff() |>
     vaccine_scenario_set_samplesize(VE_H1 = 0.6)
@@ -17,6 +19,7 @@ test_that("setting sample size works", {
   expect_true(all(Design2$n_ctrl > Design1$n_ctrl))
 
   Design3 <- vaccine_scenario() |>
+    vaccine_scenario_set_beta_A1_relative() |>
     vaccine_scenario_set_gamma_0() |>
     vaccine_scenario_set_true_eff() |>
     vaccine_scenario_set_samplesize(alpha = 0.05)
@@ -25,6 +28,7 @@ test_that("setting sample size works", {
   expect_true(all(Design3$n_ctrl < Design1$n_ctrl))
 
   Design4 <- vaccine_scenario() |>
+    vaccine_scenario_set_beta_A1_relative() |>
     vaccine_scenario_set_gamma_0() |>
     vaccine_scenario_set_true_eff() |>
     vaccine_scenario_set_samplesize(CSE=0.4)
@@ -33,6 +37,7 @@ test_that("setting sample size works", {
   expect_true(all(Design4$n_ctrl > Design1$n_ctrl))
 
   Design5 <- vaccine_scenario() |>
+    vaccine_scenario_set_beta_A1_relative() |>
     vaccine_scenario_set_gamma_0() |>
     vaccine_scenario_set_true_eff() |>
     vaccine_scenario_set_samplesize(power=0.9)
@@ -43,8 +48,10 @@ test_that("setting sample size works", {
 
 
 test_that("two functions to generate parameters output the same parameters", {
-  x <- vaccine_scenario()
-  y <- vaccine_scenario_grid()
+  x <- vaccine_scenario() |>
+    vaccine_scenario_set_beta_A1_relative()
+  y <- vaccine_scenario_grid() |>
+    vaccine_scenario_set_beta_A1_relative()
 
   expect_identical(x, y)
 })
--- tests/testthat/test-scenario_vaccine_data_generation.R
+++ tests/testthat/test-scenario_vaccine_data_generation.R
@@ -1,5 +1,6 @@
 test_that("data generation vaccine scenario", {
   Design_test <- vaccine_scenario() |>
+    vaccine_scenario_set_beta_A1_relative() |>
     vaccine_scenario_set_gamma_0() |>
     vaccine_scenario_set_true_eff() |>
     vaccine_scenario_set_samplesize()
