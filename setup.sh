2e1378cdaf1eb20ca2f40902dff5d7637562d590
Fellinger Tobias <tobias.fellinger@ages.at>
2026-04-03 10:29:01 +0200

    added git clone to setup script

--- setup.sh
+++ setup.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/sh
 
-echo installing renv
+echo cloning git repo ===============================================
+git clone -b sims_apr03 https://github.com/CI-RCT-Sim/CI.RCT.Sim.git CI.RCT.Sim
+cd CI.RCT.Sim
+
+echo installing renv ================================================
 Rscript -e "install.packages('renv')"
-echo restoring renv snapshot
+
+echo restoring renv snapshot ========================================
 Rscript -e "renv::restore()"
-echo installing package from directory
+
+echo installing package from directory ==============================
 Rscript -e "devtools::install()"
