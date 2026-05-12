f16374554b2d9862d56e00c5a01766920a2c46ea
louisejespersen <louise.jespersen@me.com>
2026-05-12 10:43:23 +0200

    Adjusted generating function and scripts are changed accordingly (#112)
    * Adjusted generating function and scripts are changed accordingly
    
    * Tests updated
    
    * Update
    
    * last update (to make the checks start running again)
    
    * Hard coded adjustment of the assumption set to accomodate for confounding
    
    * addressed fail message
    
    * true value under null hypothesis

--- tests/testthat/test-analyse_oncology_itt.R
+++ tests/testthat/test-analyse_oncology_itt.R
@@ -13,7 +13,7 @@ test_that("itt oncology works", {
   })
 
   # Generate data with no missingness, and no treatment effect, to check equivalence with linear regression
-  withr::with_seed(129, {
+  withr::with_seed(121, {
     dat <- generate_oncology(Design[50, ])
   })
   expect_no_error({
