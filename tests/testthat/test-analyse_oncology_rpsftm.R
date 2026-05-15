18ced3a1fcfdd4481db29d93ef4c32e37f52f9fa
RR <robin.ristl@meduniwien.ac.at>
2026-05-15 17:10:29 +0200

    print=FALSE removed as it is not used any more

--- tests/testthat/test-analyse_oncology_rpsftm.R
+++ tests/testthat/test-analyse_oncology_rpsftm.R
@@ -1,5 +1,5 @@
 test_that("rpsftm oncology works", {
-  Design <- oncology_scenario(print = FALSE) |>
+  Design <- oncology_scenario() |>
     oncology_scenario_set_truevalues()
 
   my_analyse <- analyse_oncology_rpsftm(recensor = TRUE)
