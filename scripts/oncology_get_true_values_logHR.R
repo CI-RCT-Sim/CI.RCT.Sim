19e3b3d6583fc0ad9a3e5d3e2d985c19a8a213a8
Robin R. <robin@ims-node2.cemsiis.meduniwien.ac.at>
2026-05-19 17:41:41 +0200

    use logHR for bias calc. as HR is biased

---
+++ scripts/oncology_get_true_values_logHR.R
@@ -0,0 +1,117 @@
+#Get true values
+
+# devtools::install()
+# renv::restore()
+# devtools::document()
+rm(list=ls())
+
+devtools::load_all()
+library(CI.RCT.Sim)
+library(parallel)
+library(survival)
+
+#Make scenario table, the code now also adds the column with scneario short names
+save_param_tab<-FALSE #need to create folder "data" if not present, as it is saved there
+source("scripts/oncology_make_scenario_table_1-2.R")
+
+#Run different scenario settings:
+#need to create folder "results" if not present!!
+#if(!file.exists("results"))  dir.create("results")
+
+#Set number of iterations
+with_random_cens<-TRUE
+pre_N_sim <- 20#0
+ev_soll_for_true_value<-100#00
+
+
+#SimClean()
+
+#scen_tab <- readxl::read_xlsx("data/oncology_scenario_list.xlsx")
+n_scenarios<-dim(scen_tab)[1]
+
+scen_set<-(1:n_scenarios)[grepl("H1",scen_tab$block_nam)]
+result_name_note<-"true_values"
+
+
+# Alpha
+alpha <- 0.05 #two sided, tests will be one-sided using alpha/2, confidence intervals are mostly hard coded to 0.95
+
+
+###################################
+
+# Derive true treatment effect -------------------------------------------
+
+sim_parameters <- oncology_scenario() |>
+  oncology_scenario_set_truevalues()
+
+
+
+
+#pre_sim_parameters <- oncology_scenario()
+
+
+pre_my_analyse <- list(
+  truth = function(condition, dat, fixed_objects = NULL) {
+    mod <- survival::coxph(Surv(time = event_time, event = ev) ~ trt + X_0 + W_0, data = dat)
+    logHR <- coef(mod)[1]
+    list(logHR = logHR)
+  }
+)
+
+pre_my_summarise <- create_summarise_function(
+  truth = summarise_estimator( #test if this can be an object defined before
+    est = logHR,
+    real = beta_death[[1]][6], #actually not required, but we need a placeholder here
+    null = 1
+  )
+)
+
+summy<-summarise_estimator( #test if this can be an object defined before
+  est = logHR,
+  real = beta_death[[1]][6], #actually not required, but we need a placeholder here
+  null = 1
+)
+pre_my_summarise <- create_summarise_function(
+  truth = summy
+)
+
+cl <- makeCluster(detectCores(logical = FALSE) - 1)
+clusterEvalQ(cl, {
+  library("CI.RCT.Sim")
+  #library("survival")
+})
+
+#SimClean()
+
+pre_results <- runSimulation(
+  design = sim_parameters[scen_set,],
+  replications = pre_N_sim,
+  generate = generate_oncology,
+  analyse = pre_my_analyse,
+  summarise = pre_my_summarise,
+  fixed_objects = list(allow_switch = FALSE, logHR_assumed = NULL, ev_soll = ev_soll_for_true_value, allow_random_cens = with_random_cens),
+  parallel = TRUE,
+  cl = cl
+)
+as.data.frame(pre_results)
+stopCluster(cl)
+pre_results$truth.mean_est
+names(pre_results)
+out<-data.frame(scen=scen_set,trueHR=pre_results$truth.mean_est)
+out
+filename<-paste("scripts/true_logHR_nsim",pre_N_sim,"_n_ev",ev_soll_for_true_value,"withRandomCens",with_random_cens,".csv",sep="")
+write.csv(out,file=filename)
+
+# Save results -----------------------------------------------------------
+#path="results/"
+#file_name<-paste(path,result_name_note,"_",hyp_select,"_",format(Sys.time(), paste0("results_onco_","nsim",N_sim,"_", Sys.info()["nodename"], "%Y-%m-%d_%H%M.Rdata")),sep="")
+#file_name
+#save(results, main_sessioninfo, nodes_sessioninfo, file = file_name)
+
+
+#results
+#A<-as.data.frame(results)
+#rej<-grepl("test.rejection_0.025",names(A))
+#cover<-grepl("est.coverage",names(A))
+#A[rej]
+#A[cover]
---
+++ scripts/oncology_make_scenario_table_1-3_logHR.R
@@ -0,0 +1,258 @@
+
+#to be run from oncology_run_all.R
+
+#devtools::load_all()
+#rm(list=ls())
+#library(CI.RCT.Sim)
+#library(parallel)
+
+#pre_sim_parameters <- oncology_scenario()
+#A<-pre_sim_parameters |> oncology_scenario_set_truevalues()
+#head(A)
+
+A<-oncology_scenario() |> oncology_scenario_set_truevalues()
+head(A)
+
+A$beta_cens
+
+#beta_lab<-c("Int","X","W","Wgrw","L","trt","switched")
+#beta_lab2<-c("Int","X","W","Wgrw","L","trt","switched","logHR_assumed")
+#beta_lab3<-c("Int","X","W","Wgrw","L","trt","switched","switching_in_control_only")
+
+
+H0<-rep(FALSE,dim(A)[1])
+for(i in 1:length(H0)) {
+ H0[i]<-A$beta_death[[i]][6]==0
+}
+H0
+small_n<-A$ev_soll==66
+
+all_param_tab<-NULL
+
+LIST<-list(
+  scen_trt_sw_H1_high=A[!H0 & small_n,],
+  scen_trt_sw_H1_low=A[!H0 & !small_n,],
+  scen_trt_sw_H0_high=A[H0 & small_n,],
+  scen_trt_sw_H0_low=A[H0 & !small_n,]
+)
+block_nam<-names(LIST)[1]
+for(block_nam in names(LIST)) {
+  tab<-LIST[[block_nam]]
+
+  #mu W:
+  temp<-tab$mu_W
+  j<-1
+  pat<-NULL
+  for(j in 1:length(temp)) {
+    pat<-c(pat,paste(temp[[j]]$trt,collapse=", "))
+    pat<-c(pat,paste(temp[[j]]$ctr,collapse=", "))
+  }
+  pat_num<-as.numeric(factor(pat))
+  muW_levels<-levels(factor(pat))
+  muW_pattern<-matrix(pat_num,ncol=2,byrow=TRUE)
+  colnames(muW_pattern)<-c("W_mean_pattern_Trt","W_mean_pattern_Ctr")
+  muW_pattern
+
+  #mu L
+  temp<-tab$mu_L
+  j<-1
+  pat<-NULL
+  for(j in 1:length(temp)) {
+    pat<-c(pat,paste(temp[[j]]$trt,collapse=", "))
+    pat<-c(pat,paste(temp[[j]]$ctr,collapse=", "))
+  }
+  pat_num<-as.numeric(factor(pat))
+  muL_levels<-levels(factor(pat))
+  muL_pattern<-matrix(pat_num,ncol=2,byrow=TRUE)
+  colnames(muL_pattern)<-c("L_mean_pattern_Trt","L_mean_pattern_Ctr")
+  muL_pattern
+
+  #W and L covariance matrix
+  temp<-tab$Sigma_W_L
+  j<-1
+  patVmat<-NULL
+  #Pat_Sigma<-NULL
+  for(j in 1:length(temp)) {
+    #Pat_Sigma<-rbind(Pat_Sigma,temp[[j]])
+    patVmat<-c(patVmat,paste(temp[[j]],collapse=", "))
+  }
+  patVmat_num<-as.numeric(factor(patVmat))
+  Sigma_levels<-levels(factor(patVmat))
+  Vmat_pattern<-matrix(patVmat_num,ncol=1,byrow=TRUE)
+  colnames(Vmat_pattern)<-c("Covariance matrix type")
+  Vmat_pattern
+
+
+  #Betas:
+
+  #beta_lab<-c("Int","X","W","Wgrw","L","trt","switched")
+  #beta_lab2<-c("Int","X","W","Wgrw","L","trt","switched","logHR_assumed")
+  #beta_lab3<-c("Int","X","W","Wgrw","L","trt","switched","switching_in_control_only")
+
+  BETA<-NULL
+  beta_names<-names(tab)[grepl("beta",names(tab))]
+  b<-beta_names[1]
+  for(b in beta_names) {
+    temp<-tab[[b]]
+    betas<-matrix(unlist(temp),ncol=length(temp[[1]]),byrow=TRUE)
+    colnames(betas)<-paste(b,names(temp[[1]]),sep=".")
+    #if(b=="beta_prog" | b=="beta_switch") colnames(betas)<-beta_lab
+    #if(b=="beta_death") colnames(betas)<-beta_lab2
+    #if(b=="beta_cens") colnames(betas)<-beta_lab3
+    #colnames(betas)<-paste(b,colnames(betas),sep=".")
+    BETA<-cbind(BETA,betas)
+  }
+  BETA<-exp(BETA) #to have Hazard ratios in table
+  #constants:
+  names(tab)
+  constant<-tab[,c("k","recr_interval","max_duration","aimed_for_n_per_required_event","alpha","power","p_trt","w")]
+
+  param_tab<-data.frame(block_nam=block_nam,muW_pattern,muL_pattern,Vmat_pattern,BETA,constant)
+
+  all_param_tab<-rbind(all_param_tab,param_tab)
+}
+
+all_param_tab<-cbind(Scen_ID=1:dim(all_param_tab)[1],all_param_tab)
+
+
+
+names(all_param_tab)
+all_param_tab$beta_cens.switching_in_control_only<-round(log(all_param_tab$beta_cens.switching_in_control_only))
+
+#names:
+names_temp<-c(
+  "Core",
+  "W - decrease under ctr",
+  "W - decrease in both groups",
+  "L - decrease under ctr, effect on death, progr., switching (unobs. conf.)",
+  "L - decrease in both groups, effect on death, progr., switching (unobs. conf.)",
+  "time dependent correlation - Toeplitz",
+  "progression - fast",
+  "progresssion - no effect of W",
+  "progression - reduced effect of trt",
+  "progression - no effect of trt",
+  "progression - unobserved confounding with death",
+  "switch - high probability",
+  "switch - no effect of W",
+  "switch - W>0 near separation",
+  "switch - unobserved confounding with death",
+  "death - high rate",
+  "death - no effect of W",
+  "death - unobserved confounding with progr. and switch",
+  "Trt effect post switch - reduced",
+  "Trt effect post switch - none",
+  "random censoring - none",
+  "random censoring - effect of X",
+  "random censoring - effect of X and W",
+  "random censoring - effect of X, W and L",
+  "random censoring - high rate, effect of X, W",
+  "random censoring - high rate, control only, effect of X, W")
+
+scenario_names<-c(
+  names_temp,
+  names_temp[names_temp!="Trt effect post switch - reduced"],
+  names_temp[!grepl("Trt effect post switch",names_temp)],
+  names_temp[!grepl("Trt effect post switch",names_temp)]
+)
+
+
+all_param_tab$scenario_name<-scenario_names
+dim2<-dim(all_param_tab)[2]
+all_param_tab<-all_param_tab[,c(1,2,dim2,3:(dim2-1))]
+head(all_param_tab)
+#scen_tab<-all_param_tab
+
+#true values were calculated using oncology_get_true_values.R, note this uses scneario tables 1-1,
+#which includes the H0 scnario with unequal trajectories for W
+#but this scenario is removed at the end of this code, because it is not really H0
+#trueHR<-read.csv("scripts/trueHR_nsim20_n_ev10000.csv") #moved to oncology_run
+trueHR<-trueHR_tab[,c("scen","trueHR")]
+names(trueHR)<-c("Scen_ID","true_eff")
+head(trueHR)
+dim(trueHR)
+dim(all_param_tab)
+scen_tab<-merge(all_param_tab,trueHR,by="Scen_ID",all.x=TRUE,all.y=FALSE)
+head(scen_tab)
+dim(scen_tab)
+scen_tab$true_eff[is.na(scen_tab$true_eff)]<-0
+
+
+A$true_eff<-scen_tab$true_eff
+sim_parameters<-A
+
+#remove H0 scenario with unequal trajectories for W
+#not needed, I changed the scenario such that W has no effect on death
+#head(scen_tab)
+#rem<-grepl("H0",scen_tab$block_nam) & grepl("W - decrease under ctr",scen_tab$scenario_name)
+#rem
+#scen_tab<-scen_tab[!rem,]
+#dim(scen_tab)
+
+if(save_param_tab) {
+  library(openxlsx)
+
+  # write dataset
+  wb <- createWorkbook()
+  addWorksheet(wb, sheetName="Scenarios")
+  writeData(wb, sheet="Scenarios", x=all_param_tab)
+
+  # define style
+  yellow_style <- createStyle(fgFill="#FFFF00")
+
+  # difference to core scenario:
+  checko<-matrix(FALSE,nrow=dim(all_param_tab)[1],ncol=dim(all_param_tab)[2])
+  b<-names(LIST)[1]
+
+  for(b in names(LIST)) {
+    set<-all_param_tab$block_nam==b
+    ind<-(1:dim(all_param_tab)[1])[set]
+    for(i in ind[-1]) {
+      different<-which(all_param_tab[i,]!=all_param_tab[ind[1],])
+      checko[i,different]<-TRUE
+    }
+  }
+
+
+
+  for(x in 1:dim(all_param_tab)[1]) {
+    for(y in 4:dim(all_param_tab)[2]) { #start at 4, because 1:3 are the ID, senario block and name and two of these are different but should not be highlighted
+      if(checko[x,y]) addStyle(wb, sheet="Scenarios", style=yellow_style, rows=x+1, cols=y, gridExpand=TRUE) # +1 for header line
+    }
+  }
+  # write result
+
+
+  #
+  addWorksheet(wb, sheetName="Mean_W_patterns")
+  Pat_muW<-data.frame(Pattern=1:length(muW_levels),Value=muW_levels)
+  writeData(wb, sheet="Mean_W_patterns", x=Pat_muW)
+
+  addWorksheet(wb, sheetName="Mean_L_patterns")
+  Pat_muL<-data.frame(Pattern=1:length(muL_levels),Value=muL_levels)
+  writeData(wb, sheet="Mean_L_patterns", x=Pat_muL)
+
+  addWorksheet(wb, sheetName="Covariance_patterns")
+  matrix(unlist(Sigma_levels),ncol=sqrt(length(Sigma_levels[[1]])))
+
+  Pat_Sigma<-NULL
+  for(i in 1:length(Sigma_levels)) {
+    xx<-Sigma_levels[i]
+    yy<-as.numeric(strsplit(xx,", ")[[1]])
+    mat<-matrix(yy,ncol=sqrt(length(yy)))
+    colnames(mat)<-paste("V",1:dim(mat)[2],sep="")
+    Pat_Sigma<-rbind(Pat_Sigma,cbind(Pattern=i,mat))
+  }
+
+  #Pat_Sigma
+  writeData(wb, sheet="Covariance_patterns", x=Pat_Sigma)
+
+  saveWorkbook(wb, paste(path,"/oncology_scenario_list.xlsx",sep=""), overwrite=TRUE)
+
+}
+
+
+
+
+
+#
+
