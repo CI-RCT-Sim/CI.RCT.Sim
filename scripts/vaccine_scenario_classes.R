220dd932d466c813f3975f2729b3045b15656c4f
Dummy_RAW-Data-Pilot_WTG <wtgrawdatapilot01@zds.ages.at>
2026-06-01 13:28:23 +0200

    all VE values in extra scenarios

--- scripts/vaccine_scenario_classes.R
+++ scripts/vaccine_scenario_classes.R
@@ -81,6 +81,14 @@ vaccine_scenario_extra <- vaccine_scenario_tweak(
   beta_W = vaccine_scenario_defaults()$beta_W[1]*c(1,-1),
   beta_AW = vaccine_scenario_defaults()$beta_AW[1]
 )
+
+vaccnine_scenario_extra$beta_A2 <- NULL
+
+vaccine_scenario_extra <- vaccine_scenario_extra |>
+  merge(y=data.frame(beta_A2=unique(scenario_A1$beta_A2)), by=NULL)
+
+
+
 # scenario_extra
 
 # Compliance:             C[+,-] + better compliance, - worse compliance
