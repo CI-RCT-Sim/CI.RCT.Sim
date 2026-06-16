69e498bace26c5f4f2479d42d22e9b40bdc72d1a
Fellinger Tobias <tobias.fellinger@ages.at>
2026-06-16 13:32:55 +0200

    new script to run vaccine sims row by row

--- run_all_vaccine_sims.sh
+++ run_all_vaccine_sims.sh
@@ -1,13 +1,25 @@
 #!/usr/bin/sh
 
-# r script reads envrionment variables
-# scenario A1, A2, ... scenario group
-# start start at chunk 1, 2, ... to re-start sims from partial results
+for i in {1..15}; do
+  scenario=A1 row=$i Rscript ./scripts/vaccine_rowwise.R
+done
 
-scenario=A1 start=1 Rscript ./scripts/vaccine_chunked.R
-scenario=A2 start=1 Rscript ./scripts/vaccine_chunked.R
-scenario=B1 start=1 Rscript ./scripts/vaccine_chunked.R
-scenario=C1 start=1 Rscript ./scripts/vaccine_chunked.R
-scenario=D1 start=1 Rscript ./scripts/vaccine_chunked.R
-scenario=extra start=1 Rscript ./scripts/vaccine_chunked.R
+for i in {1..10}; do
+  scenario=A2 row=$i Rscript ./scripts/vaccine_rowwise.R
+done
 
+for i in {1..20}; do
+  scenario=B1 row=$i Rscript ./scripts/vaccine_rowwise.R
+done
+
+for i in {1..30}; do
+  scenario=C1 row=$i Rscript ./scripts/vaccine_rowwise.R
+done
+
+for i in {1..40}; do
+  scenario=D1 row=$i Rscript ./scripts/vaccine_rowwise.R
+done
+
+for i in {1..45}; do
+  scenario=extra row=$i Rscript ./scripts/vaccine_rowwise.R
+done
