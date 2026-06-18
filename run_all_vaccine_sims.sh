591fbff53ab09f88d2006ba11ea4d063bc1c56b5
Fellinger Tobias <tobias.fellinger@ages.at>
2026-06-18 09:57:00 +0200

    added package setup to sim bash script
    as proposed by Louise

--- run_all_vaccine_sims.sh
+++ run_all_vaccine_sims.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/sh
 
+echo installing renv
+Rscript -e "install.packages('renv')"
+Rscript -e "renv::restore(packages = 'renv')"
+
+echo restoring renv snapshot
+Rscript -e "renv::restore()"
+
+echo installing devtools
+Rscript -e "install.packages('devtools')"
+
+echo installing package from directory
+Rscript -e "devtools::install()"
+
 for i in {1..15}; do
   scenario=A1 row=$i RENV_CONFIG_SANDBOX_ENABLED=FALSE Rscript ./scripts/vaccine_rowwise.R
 done
